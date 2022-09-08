d6667d576e6fa8c3b645ef139c978d46437af457
Rob Oates <roates@gmail.com>
2022-09-08 10:38:09 +0100

    added accessibility using js. 1. event.preventDefault to disable back button and jump to top default of menu open and close 2. add and use aria-expanded via js 3. using aria-expanded as states to style open and close states this allows basic functionality using css only, and fixes the back / top issues and adds accessibility using js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+document.querySelector(".menu-open").addEventListener("click", function(event){
+    event.preventDefault();
+    document.querySelector("#menu").setAttribute("aria-expanded","true");
+});
+document.querySelector(".menu-close").addEventListener("click",function(event){
+    event.preventDefault();
+    document.querySelector("#menu").setAttribute("aria-expanded","false");
+});
