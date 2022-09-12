db16cf28bdcdad0d9de29d3d9c434fe8aa68c0e2
Rob Oates <roates@gmail.com>
2022-09-12 11:37:39 +0100

    removed previous change, added state classes for open and close buttons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 document.querySelector(".menu-open").addEventListener("click", function(event){
     event.preventDefault();
     document.querySelector("#menu").setAttribute("aria-expanded","true");
+    document.querySelector(".menu-open").classList.add("active");
+    document.querySelector(".menu-close").classList.remove("active");
 });
 document.querySelector(".menu-close").addEventListener("click",function(event){
     event.preventDefault();
     document.querySelector("#menu").setAttribute("aria-expanded","false");
+    document.querySelector(".menu-close").classList.add("active");
+    document.querySelector(".menu-open").classList.remove("active");
 });
