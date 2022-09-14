f06d668c829e2f429c365f15d5d3cd8e38cf8118
Katrina <katrina@jackalbookpro-4.home>
2022-09-14 08:00:37 +0100

    added animations to menu open and close

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.querySelector(".menu-open").addEventListener("click", function(event){
     event.preventDefault();
     document.querySelector("#menu").setAttribute("aria-expanded","true");
-    document.querySelector(".menu-close").classList.add("active");
-    document.querySelector(".menu-open").classList.remove("active");
+    document.querySelector(".menu-close a").classList.add("active");
+    document.querySelector(".menu-open a").classList.remove("active");
 });
 document.querySelector(".menu-close").addEventListener("click",function(event){
     event.preventDefault();
     document.querySelector("#menu").setAttribute("aria-expanded","false");
-    document.querySelector(".menu-open").classList.add("active");
-    document.querySelector(".menu-close").classList.remove("active");
+    document.querySelector(".menu-open a").classList.add("active");
+    document.querySelector(".menu-close a").classList.remove("active");
 });
